94283e795f4e2456e23a8a5086c48ad7338f169d
zhongming <zhongming139@126.com>
2026-07-04 08:53:01 +0800

    font issue

--- src/main/java/zm/mud/ui/util/AnsiToStyleDocUtil.java
+++ src/main/java/zm/mud/ui/util/AnsiToStyleDocUtil.java
@@ -18,8 +18,8 @@ public class AnsiToStyleDocUtil {
     public void parseAnsiToStyledDocument(String text, StyledDocument doc, Font font, ITheme theme,boolean enableBlod) throws BadLocationException {
         if (text == null || text.isEmpty()) return;
         
-        SimpleAttributeSet currentAttr = new SimpleAttributeSet();
-        StyleConstants.setFontFamily(currentAttr, font.getName());
+        SimpleAttributeSet currentAttr = new SimpleAttributeSet(); 
+        StyleConstants.setFontFamily(currentAttr, font.getFamily());
         StyleConstants.setFontSize(currentAttr, font.getSize());
         
         // 增加两个状态变量，用于记录当前“最原始”的颜色属性，避免 contrast 重复计算导致颜色污染
