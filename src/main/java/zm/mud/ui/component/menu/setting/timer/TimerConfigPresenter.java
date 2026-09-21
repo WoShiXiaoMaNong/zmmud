d222a32e17595ee4a5dffd9b18069b3bf0f6b6ea
zhongming <zhongming139@126.com>
2026-09-21 23:00:45 +0800

    初始化timer

---
+++ src/main/java/zm/mud/ui/component/menu/setting/timer/TimerConfigPresenter.java
@@ -0,0 +1,17 @@
+package zm.mud.ui.component.menu.setting.timer;
+
+public class TimerConfigPresenter {
+
+    public TimerConfigPresenter(TimerConfigView configView) {
+        //TODO Auto-generated constructor stub
+    }
+
+    public void loadConfig() {
+        
+    }
+
+    public void saveConfig() {
+       
+    }
+    
+}
---
+++ src/main/java/zm/mud/ui/component/menu/setting/timer/TimerConfigView.java
@@ -0,0 +1,7 @@
+package zm.mud.ui.component.menu.setting.timer;
+
+import javax.swing.JPanel;
+
+public class TimerConfigView extends JPanel{
+    
+}
